2ab13f827c5fef10c22947b78ebc85f52f9582d8
Giorgos Dimtsas <gedim21@gmail.com>
2015-04-28 14:02:29 +0300

    Removed bootstrap and jQuery and replaced them with WebJars dependencies.

--- src/main/resources/META-INF/load-script.sql
+++ src/main/resources/META-INF/load-script.sql
@@ -1,5 +1,5 @@
-insert into author(id, name, email) values(1, 'Giorgos Dimtsas', 'gedim21@gmail.com');
-insert into author(id, name, email) values(2, 'Nefeli Rovithi', 'nefeli.rovithi@gmail.com');
+insert into author(id, name, email) values(1, 'Author 1', 'author1@gmail.com');
+insert into author(id, name, email) values(2, 'Author 2', 'author2@gmail.com');
 
 insert into post(id, title, content, author_id) values(1, 'First post', 'This is my first post', 1);
 insert into post(id, title, content, author_id) values(2, 'Second post', 'This is my second post', 2);
